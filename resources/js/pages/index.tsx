454aa795c8fd0d89fb609163caed5fbdc1d02b11
JosephVTX <jvcallupe69@gmail.com>
2024-06-04 00:35:33 -0500

    ...

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -5,63 +5,17 @@ export default function index() {
     const { count, increment, decrement } = useCounterStore();
     return (
         <DashboardLayout>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-            sapiente quod tempora dicta asperiores magnam officiis corporis
-            voluptatibus. Fugit odit necessitatibus consectetur blanditiis
-            molestias nostrum itaque minus hic sapiente at excepturi, laborum
-            velit. Unde error odio impedit, at ullam quas rem sequi ea quasi,
-            ipsam sint, ipsum vero molestiae minus. Nostrum dolores temporibus
-            aperiam beatae at cumque quam quasi adipisci laboriosam harum
-            voluptatum voluptates incidunt nihil numquam, alias ut consequuntur
-            sapiente corporis quia laborum magni totam! Non nesciunt porro animi
-            aliquid vel adipisci sint ipsa nemo optio illum quidem assumenda
-            iusto iure similique est quibusdam, dolore eaque obcaecati molestias
-            accusantium cum in omnis at perspiciatis. Totam porro nam optio
-            vitae explicabo. Delectus expedita rerum deserunt voluptatem
-            recusandae repudiandae. Nihil, voluptate expedita itaque totam nobis
-            amet assumenda rem necessitatibus tempore vel tenetur ipsam non
-            labore. Nemo amet qui nam, commodi, repellat corrupti illum totam
-            ut, aliquid nobis earum blanditiis quae nihil culpa architecto
-            fugiat! Omnis possimus natus ipsa a nesciunt molestiae dolor
-            laborum, provident nobis, quibusdam illo esse beatae expedita odio!
-            Hic amet dolores ea accusamus perspiciatis enim, nam voluptatum
-            impedit quia debitis aliquam dicta iusto beatae autem. Consectetur
-            libero debitis dolores ipsa exercitationem. Quos, reiciendis.
-            Expedita nostrum est, libero recusandae ex eveniet rem, maxime totam
-            asperiores sapiente tenetur placeat, possimus quasi quaerat! Enim
-            maxime illo quidem! Eos quia ex quae saepe quo, cumque, quam labore
-            vero sit molestiae asperiores aliquid nisi sunt nam. Atque eius
-            doloremque iure soluta necessitatibus aut dolor expedita deleniti
-            aliquid voluptatum quod et deserunt, ipsam impedit quasi enim quis
-            totam, sequi veritatis vel eveniet reprehenderit. Neque nam dolorum
-            suscipit nihil non minima explicabo officiis molestiae deleniti
-            esse. Quos repellat, soluta esse laudantium quibusdam voluptate quam
-            itaque accusamus optio saepe eos ullam accusantium reprehenderit
-            ipsum, animi iure dolores illo dignissimos tempore asperiores.
-            Similique, in. Aliquid libero, minus cupiditate blanditiis,
-            similique quos doloribus ipsa minima voluptates autem natus! Nostrum
-            amet odit atque debitis, numquam voluptatum neque nobis accusantium
-            necessitatibus, consequuntur eaque inventore, impedit libero
-            sapiente porro doloremque! Harum explicabo laborum voluptate
-            dolorum. Rem rerum harum, explicabo autem nulla, esse, expedita
-            vitae molestias adipisci natus aut dignissimos sint ea sunt. Totam
-            autem est voluptate voluptas beatae porro, ratione dolores
-            necessitatibus optio accusantium quod sequi! Autem totam quidem ut
-            saepe quaerat adipisci necessitatibus hic, doloribus est voluptates,
-            eum ipsam quis ea earum aliquam debitis! Fugit delectus et eveniet
-            illum sed, sunt sint! Repudiandae, quod, voluptate eaque amet nobis
-            totam laborum necessitatibus asperiores eius non adipisci molestias
-            aliquid quasi eos nulla debitis autem labore. Hic minima quod
-            tenetur delectus fugit totam voluptatem dignissimos cum debitis unde
-            assumenda sit molestias a earum quas corrupti consequatur eligendi
-            suscipit molestiae, odio ratione dicta nobis omnis. Esse maxime
-            excepturi deserunt, vero rerum quaerat rem impedit laudantium
-            similique fugit, exercitationem dolor, eligendi minima quam. Rem
-            labore minus est eos maiores tempore, quam a sequi error quos
-            praesentium, cumque, aliquid quidem eaque enim ut mollitia itaque
-            ab? Aperiam repellat, molestiae non possimus fuga excepturi sunt
-            tempora, sint corporis aspernatur numquam architecto quam.
-            Aspernatur rerum molestiae ullam necessitatibus!
+            <form>
+                <input
+                    type="text"
+                    placeholder="Some Text..."
+                    className="form-input"
+                    required
+                />
+                <button type="submit" className="btn btn-primary mt-6">
+                    Submit
+                </button>
+            </form>
         </DashboardLayout>
     );
 }
